9674f828a67c42124cec54d2a88cecb69092284a
Đorđe Milanović <djordje200179@gmail.com>
2024-07-24 14:56:40 +0200

    Minor change

--- Minesweeper/Board.fs
+++ Minesweeper/Board.fs
@@ -21,22 +21,20 @@ let RevealBoard (board: Board) (y: int, x: int): Board =
     let rec OpenCell (tiles: Cell [, ]) (y: int, x: int) =
         let height, width = Array2D.length1 board.Tiles, Array2D.length2 board.Tiles
 
-        if tiles[y, x] = Closed then
+        if x >= 0 && x < width && y >= 0 && y < height && tiles.[y, x] = Closed then
             tiles[y, x] <- Opened
 
             if board.Minefield[y, x] = Clear then
-                if y > 0 then OpenCell tiles (y - 1, x)
-                if y < height - 1 then OpenCell tiles (y + 1, x)
-                if x > 0 then OpenCell tiles (y, x - 1)
-                if x < width - 1 then OpenCell tiles (y, x + 1)
-                if y > 0 && x > 0 then OpenCell tiles (y - 1, x - 1)
-                if y > 0 && x < width - 1 then OpenCell tiles (y - 1, x + 1)
-                if y < height - 1 && x > 0 then OpenCell tiles (y + 1, x - 1)
-                if y < height - 1 && x < width - 1 then OpenCell tiles (y + 1, x + 1)
+                OpenCell tiles (y - 1, x)
+                OpenCell tiles (y + 1, x)
+                OpenCell tiles (y, x - 1)
+                OpenCell tiles (y, x + 1)
+                OpenCell tiles (y - 1, x - 1)
+                OpenCell tiles (y - 1, x + 1)
+                OpenCell tiles (y + 1, x - 1)
+                OpenCell tiles (y + 1, x + 1)
 
     let newTiles = Array2D.copy board.Tiles
     OpenCell newTiles (y, x)
 
-    {
-        board with Tiles = newTiles
-    }
+    { board with Tiles = newTiles }
