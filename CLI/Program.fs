e3910645207edc43ecaec83905c3bce7163582d9
Đorđe Milanović <djordje200179@gmail.com>
2024-07-24 14:53:04 +0200

    Initial commit

---
+++ CLI/Program.fs
@@ -0,0 +1,24 @@
+﻿open Minesweeper.Board
+open Minesweeper.Minefield
+
+let PrintBoard (board: Board) =
+    let height, width = Array2D.length1 board.Tiles, Array2D.length2 board.Tiles
+
+    for y in 0..height-1 do
+        for x in 0..width-1 do
+            if board.Tiles.[y, x] = Opened then
+                match board.Minefield[y, x] with
+                | Clear -> printf " "
+                | Mine -> printf "X"
+                | NearMine n -> printf "%d" n
+            else
+                printf "#"
+        printfn ""
+
+let board = GenerateBoard (10, 10) 10
+PrintBoard board
+
+printfn ""
+
+let board2 = RevealBoard board (2, 2)
+PrintBoard board2
---
+++ Minesweeper/Board.fs
@@ -0,0 +1,42 @@
+﻿module Minesweeper.Board
+
+open Minesweeper.Minefield
+
+type Cell =
+    | Opened
+    | Closed
+
+type Board = {
+    Tiles: Cell [,]
+    Minefield: Field
+}
+
+let GenerateBoard (height: int, width: int) (mines: int): Board =
+    let field = GenerateField (height, width) mines
+    let tiles = Array2D.create height width Closed
+
+    { Tiles = tiles; Minefield = field }
+
+let RevealBoard (board: Board) (y: int, x: int): Board = 
+    let rec OpenCell (tiles: Cell [, ]) (y: int, x: int) =
+        let height, width = Array2D.length1 board.Tiles, Array2D.length2 board.Tiles
+
+        if tiles[y, x] = Closed then
+            tiles[y, x] <- Opened
+
+            if board.Minefield[y, x] = Clear then
+                if y > 0 then OpenCell tiles (y - 1, x)
+                if y < height - 1 then OpenCell tiles (y + 1, x)
+                if x > 0 then OpenCell tiles (y, x - 1)
+                if x < width - 1 then OpenCell tiles (y, x + 1)
+                if y > 0 && x > 0 then OpenCell tiles (y - 1, x - 1)
+                if y > 0 && x < width - 1 then OpenCell tiles (y - 1, x + 1)
+                if y < height - 1 && x > 0 then OpenCell tiles (y + 1, x - 1)
+                if y < height - 1 && x < width - 1 then OpenCell tiles (y + 1, x + 1)
+
+    let newTiles = Array2D.copy board.Tiles
+    OpenCell newTiles (y, x)
+
+    {
+        board with Tiles = newTiles
+    }
